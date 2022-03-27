851942591802bb4669dbd274a71641ed96d8e8a2
Esa Firman <esafirm21@gmail.com>
2022-03-27 22:10:31 +0700

    add assertion for the spec

--- lib/src/test/kotlin/nolambda/stream/cleaningservice/plugin/components/CleaningFromReportSpec.kt
+++ lib/src/test/kotlin/nolambda/stream/cleaningservice/plugin/components/CleaningFromReportSpec.kt
@@ -1,6 +1,8 @@
 package nolambda.stream.cleaningservice.plugin.components
 
+import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import nolambda.stream.cleaningservice.remover.DeleteActionRemover
 import nolambda.stream.cleaningservice.report.DefaultReportParser
 import nolambda.stream.cleaningservice.utils.SimpleLogger
@@ -24,5 +26,26 @@ class CleaningFromReportSpec : StringSpec({
 
     "Scenario must run successfully" {
         remover.remove(actions)
+
+        val removedFiles = listOf(
+            "myawesomemodule1/src/main/res/values/strings-unused.xml",
+            "myawesomemodule2/src/main/res/layout/acitivty_unused.xml"
+        )
+
+        val removedItemFiles = listOf(
+            "myawesomemodule1/src/main/res/values/strings.xml"
+        )
+
+        removedFiles.forEach {
+            File(sampleDir, it).exists() shouldBe false
+        }
+
+        removedItemFiles.forEach {
+            File(sampleDir, it).exists() shouldBe true
+        }
     }
-})
+}) {
+    override fun afterSpec(spec: Spec) {
+        // Do git reset head hard for sample
+    }
+}
